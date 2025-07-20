b3055d72ebe527e0ceb6285d455233e642697051
hosamksbaa <hosamksbaa@gmail.com>
2025-07-20 07:16:21 +0300

    as

--- lib/hosamAddition/HttpReqstats/ErrorLogic.dart
+++ lib/hosamAddition/HttpReqstats/ErrorLogic.dart
@@ -15,19 +15,19 @@ class ApiErrorChecker {
         print("Response contains 'msg' and 'send' keys.");
         if (responseData['msg'] != 'ok' || responseData['send'] != 'ok') {
           String message = "Message: ${responseData['msg']}, Send: ${responseData['send']}";
-          HDMMsg.showSnackBar(title: 'Notice', message: message, contentType: ContentType.warning);
+          // HDMMsg.showSnackBar(title: 'Notice', message: message, contentType: ContentType.warning);
           print("Condition not met: msg='${responseData['msg']}' or send='${responseData['send']}'");
           return false;
         }
       } else if (responseData is Map && responseData.containsKey('msg') && responseData['msg'] != 'ok') {
-        HDMMsg.showSnackBar(title: 'Error', message: "Error in msg: ${responseData['msg']}", contentType: ContentType.failure);
+        // HDMMsg.showSnackBar(title: 'Error', message: "Error in msg: ${responseData['msg']}", contentType: ContentType.failure);
         print("Error found in 'msg' key: ${responseData['msg']}");
         return false;
       }
     } 
     else {
       String errorMessage = "Error: Status code is ${response.response.statusCode}. Body: ${response.response.data}";
-      HDMMsg.showSnackBar(title: 'API Error', message: errorMessage, contentType: ContentType.failure);
+      // HDMMsg.showSnackBar(title: 'API Error', message: errorMessage, contentType: ContentType.failure);
       print("API Error: Status code not 200. Error message: $errorMessage");
       return false;
     }
