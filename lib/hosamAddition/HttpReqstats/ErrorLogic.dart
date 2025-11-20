84eeac63138e090046b37b51be1f001ca648e54e
HosamKsbaa <hosamksbaa@gmail.com>
2025-11-20 17:44:59 +0200

    Update: accept HTTP status codes 200-300 as successful responses
    - Changed error checking to accept any status code from 200-299 (2xx responses)
    - Updated to use dio.Response instead of retrofit.HttpResponse
    - Improved error messages to reflect the new status code range

--- lib/hosamAddition/HttpReqstats/ErrorLogic.dart
+++ lib/hosamAddition/HttpReqstats/ErrorLogic.dart
@@ -1,29 +1,30 @@
-import 'package:retrofit/retrofit.dart' as retrofit;
+import 'package:dio/dio.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 class ApiErrorChecker {
   static final _talker = TalkerFlutter.init();
 
-  static bool checkResponse<T>(retrofit.HttpResponse<T> response) {
-    if (response.response.statusCode == 200) {
-      var responseData = response.response.data;
+  static bool checkResponse<T>(Response<T> response) {
+    // Accept any status code from 200 to 300 (successful responses)
+    if (response.statusCode != null && response.statusCode! >= 200 && response.statusCode! < 300) {
+      var responseData = response.data;
 
       if (responseData is Map && responseData.containsKey('msg') && responseData.containsKey('send')) {
         _talker.debug("Response contains 'msg' and 'send' keys.");
         if (responseData['msg'] != 'ok' || responseData['send'] != 'ok') {
-          // HDMMsg.showSnackBar(title: 'Notice', message: message, contentType: ContentType.warning);
+          // hdmMsg.showSnackBar(title: 'Notice', message: message, contentType: ContentType.warning);
           _talker.warning("Condition not met: msg='${responseData['msg']}' or send='${responseData['send']}'");
           return false;
         }
       } else if (responseData is Map && responseData.containsKey('msg') && responseData['msg'] != 'ok') {
-        // HDMMsg.showSnackBar(title: 'Error', message: "Error in msg: ${responseData['msg']}", contentType: ContentType.failure);
+        // hdmMsg.showSnackBar(title: 'Error', message: "Error in msg: ${responseData['msg']}", contentType: ContentType.failure);
         _talker.error("Error found in 'msg' key: ${responseData['msg']}");
         return false;
       }
     } else {
-      String errorMessage = "Error: Status code is ${response.response.statusCode}. Body: ${response.response.data}";
-      // HDMMsg.showSnackBar(title: 'API Error', message: errorMessage, contentType: ContentType.failure);
-      _talker.error("API Error: Status code not 200. Error message: $errorMessage");
+      String errorMessage = "Error: Status code is ${response.statusCode}. Body: ${response.data}";
+      // hdmMsg.showSnackBar(title: 'API Error', message: errorMessage, contentType: ContentType.failure);
+      _talker.error("API Error: Status code not in 200-299 range. Error message: $errorMessage");
       return false;
     }
     return true;
